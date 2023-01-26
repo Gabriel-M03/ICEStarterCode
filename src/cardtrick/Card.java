dec81e6876f9db335fd2e7718d461868e19b2d86
Gabriel-M03 <montemga@sheridancollege.ca>
2023-01-25 19:39:23 -0500

    ICE MK 2

--- src/cardtrick/Card.java
+++ src/cardtrick/Card.java
@@ -5,42 +5,42 @@ package cardtrick;
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
- * @author dancye
- * @author Paul Bonenfant May 2020
+ * modifier Gabriel
+ * @author Gabriel Montemayor
+ * this is a branch
  */
 public class Card 
 {
-     private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+    private String suit; //clubs, spades, diamonds, hearts
+    private int value;//1-13
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
      * @return the suit
      */
-    public String getSuit() {
+    public String getSuit() 
+    {
         return suit;
     }
-
     /**
      * @param suit the suit to set
      */
-    public void setSuit(String suit) {
+    public void setSuit(String suit) 
+    {
         this.suit = suit;
     }
-
     /**
      * @return the value
      */
-    public int getValue() {
+    public int getValue() 
+    {
         return value;
     }
-
     /**
      * @param value the value to set
      */
-    public void setValue(int value) {
+    public void setValue(int value) 
+    {
         this.value = value;
-    }
-     
+    }    
 }
--- src/cardtrick/CardTrick.java
+++ src/cardtrick/CardTrick.java
@@ -1,7 +1,8 @@
 package cardtrick;
-
+import java.util.Scanner;
 /**
  * This class +++ Insert class description here +++
+ *
  * @author Gabriel Montemayor
  */
 public class CardTrick 
@@ -9,55 +10,61 @@ public class CardTrick
     public static void main(String[] args) 
     {
         Card[] hand = new Card[7];
-
-        for (int i = 0; i < hand.length; i++) 
-        {
-            Card card = new Card();
-            card.setValue((int)(1 + Math.random() * 14));
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
-            hand [i] = card;
-                System.out.println();
+        Scanner input = new Scanner(System.in);
+        int i = 0, a = 0;
+        Card card = new Card();
+        for (i = 0; i < hand.length; i++) {
+            card.setValue((int) (1 + Math.random() * 14));
+            card.setSuit(Card.SUITS[(int) (0 + Math.random() * 3)]);
+            hand[i] = card;
+            System.out.println(hand[i].getValue() + " " + hand[i].getSuit());
         }
+        System.out.println("Enter a suit type");
+        String s1 = input.next();
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.        
+        System.out.println();
+        System.out.println("Enter a random number 1 - 14:");
+        System.out.println("Either: Clubs, Spades, Diamonds & Hearts");
+        int num = input.nextInt();
+        for (i = 0; i < hand.length; i++) 
+        {
+            if(hand[i].getValue() == num || hand[i].getSuit().equalsIgnoreCase(s1)) 
+            {
+                printInfo();
+                break;
+            } 
+            else 
+            {
+                System.out.println("Sorry - array has no match to entry");
+                break;
+            }
+        }        
     }
-    
     /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
      * @author Paul Bonenfant Jan 2022
      */
     private static void printInfo() 
-    {   
+    {
+
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
-        
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+
+        System.out.println("My name is Gabriel, but you can call me Gabe.");
         System.out.println();
-        
+
         System.out.println("My career ambitions:");
         System.out.println("-- Be more active on LinkedIn");
+        System.out.println("-- Get into Sheridan COOP");
+        System.out.println("-- Graduate Sheridan");
+        System.out.println("-- Get into cyber security either for the government or corporate");
         System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
+        System.out.println();
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
-
-        System.out.println(); 
+        System.out.println("-- Instruments/Music Production");
+        System.out.println("-- Sports/Working out");
+        System.out.println("-- Anime/Watching TV");
+        System.out.println("-- Driving");
+        System.out.println();
     }
-
 }
